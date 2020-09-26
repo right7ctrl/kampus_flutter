bde26ec18eac8b5390904decb2c310cdad126ad7
Murat Aslan <murataslanx33@gmail.com>
2020-09-26 23:51:16 +0300

    project structure

---
+++ lib/controllers/auth/login_controller.dart
@@ -0,0 +1,24 @@
+import 'package:get/get.dart';
+
+class LoginController extends GetxController {
+  @override
+  void onInit() {
+    // called immediately after the widget is allocated memory
+    //fetchApi();
+    super.onInit();
+  }
+
+  @override
+  void onReady() {
+    // called after the widget is rendered on screen
+    //showIntroDialog();
+    super.onReady();
+  }
+
+  @override
+  void onClose() {
+    // called just before the Controller is deleted from memory
+    //closeStream();
+    super.onClose();
+  }
+}
--- lib/views/auth/login_page.dart
+++ lib/views/auth/login_page.dart
@@ -1,8 +1,153 @@
+import 'package:chat_app_flutter/controllers/auth/login_controller.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
 class LoginPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return GetBuilder<LoginController>(
+      // specify type as Controller
+      init: LoginController(), // intialize with the Controller
+      builder: (value) => Scaffold(
+        body: Stack(
+          children: [
+            Material(
+              elevation: 8,
+              borderRadius: BorderRadius.only(),
+              shadowColor: Colors.white38,
+              child: Container(
+                width: MediaQuery.of(context).size.width,
+                height: MediaQuery.of(context).size.height + 200,
+                decoration: BoxDecoration(
+                  image: DecorationImage(
+                      image: AssetImage("assets/images/wp1.jpg"),
+                      fit: BoxFit.cover),
+                  borderRadius: BorderRadius.only(),
+                ),
+              ),
+            ),
+            Positioned(
+              bottom: 0,
+              child: Padding(
+                padding: const EdgeInsets.only(bottom: 0),
+                child: Material(
+                  color: Colors.transparent,
+                  borderRadius: BorderRadius.only(
+                    bottomLeft: Radius.circular(32),
+                    bottomRight: Radius.circular(32),
+                  ),
+                  child: Container(
+                    padding: EdgeInsets.only(bottom: 54),
+                    width: MediaQuery.of(context).size.width,
+                    decoration: BoxDecoration(
+                      gradient: LinearGradient(
+                          begin: Alignment.bottomLeft,
+                          end: Alignment.topRight,
+                          stops: [
+                            0,
+                            .7,
+                          ],
+                          colors: [
+                            Color(0xFFFd07d6b),
+                            Color(0xFFFdda196),
+                          ]),
+                      borderRadius: BorderRadius.only(
+                        topLeft: Radius.circular(32),
+                        topRight: Radius.circular(32),
+                      ),
+                    ),
+                    child: Padding(
+                      padding: const EdgeInsets.only(top: 30),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Text(
+                            "Her zaman,",
+                            style: TextStyle(
+                                fontWeight: FontWeight.bold,
+                                color: Colors.white,
+                                fontSize: 28),
+                          ),
+                          Text(
+                            "heryerde, birlikte olun.",
+                            style: TextStyle(
+                                fontWeight: FontWeight.bold,
+                                color: Colors.white,
+                                fontSize: 28),
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.only(top: 20),
+                            child: Text(
+                              "Tek bir yerde mesajlaşın, görüntülü sohbet edin,",
+                              style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 12,
+                              ),
+                            ),
+                          ),
+                          Text(
+                            "birlikte bir şeyler planlayın.",
+                            style: TextStyle(color: Colors.white, fontSize: 12),
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.only(top: 40.0),
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                              crossAxisAlignment: CrossAxisAlignment.center,
+                              children: [
+                                GestureDetector(
+                                  onTap: () {
+                                   
+                                  },
+                                  child: Container(
+                                    width: 140,
+                                    height: 45,
+                                    decoration: BoxDecoration(
+                                      border: Border.all(
+                                          color: Colors.white, width: 1.5),
+                                      borderRadius: BorderRadius.circular(25),
+                                    ),
+                                    child: Center(
+                                      child: Text(
+                                        "Oturum Aç",
+                                        style: TextStyle(
+                                            color: Colors.white,
+                                            fontWeight: FontWeight.w500),
+                                      ),
+                                    ),
+                                  ),
+                                ),
+                                Container(
+                                  width: 140,
+                                  height: 45,
+                                  decoration: BoxDecoration(
+                                    color: Colors.white,
+                                    border: Border.all(
+                                        color: Colors.white, width: 1.5),
+                                    borderRadius: BorderRadius.circular(25),
+                                  ),
+                                  child: Center(
+                                    child: Text(
+                                      "Kaydol",
+                                      style: TextStyle(
+                                          color: Colors.black,
+                                          fontWeight: FontWeight.w500),
+                                    ),
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
